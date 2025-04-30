9fcdc05ed14556f0499ed76f54a82ee0a0fbcdf6
王永山 <lygwys@outlook.com>
2025-05-01 06:13:15 +0800

    添加百度移动统计 MauiLib1 项目及多平台支持 在解决方案中添加了新的项目 `MauiLib1`，并为其配置了调试和发布设置。更新了项目文件以引用新项目，并设置了多个目标框架（Android、iOS、Mac Catalyst 和 Windows），启用了 MAUI 支持。
    在 `AndroidManifest.xml` 中添加了必要的属性和百度统计的元数据。在 `MainActivity.cs` 中实现了存储权限的检查和请求，并初始化了百度统计服务。
    
    同时，在 `AboutPage.xaml` 中更新了打赏提示文本和操作步骤。为不同平台添加了 `PlatformClass1` 类，以支持平台特定的实现。

--- Notes/Platforms/Android/MainActivity.cs
+++ Notes/Platforms/Android/MainActivity.cs
@@ -1,10 +1,28 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
+using Com.Baidu.Mobstat;
 
 namespace Notes
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // 检查并请求存储权限  
+            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.WriteExternalStorage) != Permission.Granted)
+            {
+                ActivityCompat.RequestPermissions(this, new string[] { Manifest.Permission.WriteExternalStorage }, 1);
+            }
+
+            // 百度统计
+            StatService.Start(this);
+        }
     }
 }
